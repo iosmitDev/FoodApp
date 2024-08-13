03a2a10fbbe4094b5ac80edf267c59d5138b919f
miteshpate-admin <Mitesh.imply@gmail.com>
2024-08-14 03:18:45 +0530

    Added Outlet in View

--- FoodAppSwiftJuly/Extensions/UIView+Extension.swift
+++ FoodAppSwiftJuly/Extensions/UIView+Extension.swift
@@ -5,4 +5,19 @@
 //  Created by MiteshKumar Patel on 14/08/24.
 //
 
-import Foundation
+import UIKit
+
+//Create Extension for corner radius so we need to extend UIView
+extension UIView {
+    
+    //Computed property , define like variable but it returns value
+  @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+    
+}
--- FoodAppSwiftJuly/Views/Onboarding/OnboardingViewController.swift
+++ FoodAppSwiftJuly/Views/Onboarding/OnboardingViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-  
+    
+    @IBAction func nextButtonClicked(_ sender: UIButton) {
+        
+        
+    }
+        
 }
