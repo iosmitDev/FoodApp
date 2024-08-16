c86bc7e59343fd98e64808778db2a2db0a67600c
miteshpate-admin <Mitesh.imply@gmail.com>
2024-08-16 19:04:59 +0530

    "Created a new branch and changes in HomeVC"

--- FoodAppSwiftJuly/Views/Home/HomeViewController.swift
+++ FoodAppSwiftJuly/Views/Home/HomeViewController.swift
@@ -35,7 +35,7 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        //Created New branch
         //title = "Yummie"
       registerCollectionViewCell()
     }
